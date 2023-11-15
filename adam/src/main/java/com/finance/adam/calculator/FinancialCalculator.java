23b6f69de17e290e0cb9b5b1378eda7b23f4c50e
JMsuper <tjsqls2067@naver.com>
2023-11-15 19:54:55 +0900

    욕심을 버리고, 작은 기능부터 다시 시작

--- adam/src/main/java/com/finance/adam/calculator/FinancialCalculator.java
+++ adam/src/main/java/com/finance/adam/calculator/FinancialCalculator.java
@@ -8,6 +8,11 @@ import java.util.Map;
 public class FinancialCalculator {
     public double expectedReturnCal(Map financeInfo){return 0;}
 
+    // ROE = (EPS/BPS) * 100
+    static public double calculateBPS(double EPS, double ROE){
+        return (EPS*100)/ROE;
+    }
+
 }
 
 
--- adam/src/main/java/com/finance/adam/manager/controller/ManagerController.java
+++
@@ -1,7 +0,0 @@
-package com.finance.adam.manager.controller;
-
-
-// 모니터링 프로세스, 메일 프로세스와 통신하기 위함
-public class ManagerController {
-
-}
