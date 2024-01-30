abf8c8fd47a9d90aa6c2c70dd3476b68eee3e536
JMsuper <tjsqls2067@naver.com>
2024-01-30 12:14:19 +0900

    [test] Open Dart 테스트

--- adam/src/test/java/com/finance/adam/openapi/TestOpenDartAPI.java
+++ adam/src/test/java/com/finance/adam/openapi/TestOpenDartAPI.java
@@ -2,11 +2,15 @@ package com.finance.adam.openapi;
 
 import com.finance.adam.openapi.dart.OpenDartAPI;
 import com.finance.adam.openapi.dart.vo.OpenDartFinancialInfo;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class TestOpenDartAPI {
@@ -15,14 +19,25 @@ public class TestOpenDartAPI {
     OpenDartAPI openDartAPI;
 
     @Test
-    void test(){
+    @DisplayName("Open Dart 단일기업 재무정보 조회 확인")
+    void test1(){
 
         String corpCode = "00126380";
         String bsnsYear = "2022";
 
         List<OpenDartFinancialInfo> result = openDartAPI.getCorpFinancialInfo(corpCode,bsnsYear);
-        for (OpenDartFinancialInfo info : result){
-            System.out.println(info);
-        }
+        assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("Open Dart 공시 기업코드 조회 확인(삼성전자 종목코드 활용)")
+    void test2(){
+        Map<String, String> map = openDartAPI.getCorpCodeMap();
+
+        String SAMSUNG_CORP_CODE = "00126380";
+        String SAMSUNG_STOCK_CODE = "005930";
+
+        assertTrue(map.size() > 3000);
+        assertEquals(SAMSUNG_CORP_CODE,map.get(SAMSUNG_STOCK_CODE));
     }
 }
