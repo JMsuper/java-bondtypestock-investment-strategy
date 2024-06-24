667ce7392515aa637fc0c55c360eb05a6292978e
JMsuper <tjsqls2067@naver.com>
2024-06-24 18:09:43 +0900

    [Refactor] '/screening' , 'stockInfos' , 'financeInfo' API URI 변경
    - '/screening/step1' -> '/api/v1/finances/screening'
    - '/stockInfos' -> '/api/v1/finances/stocks'
    - '/financeInfo/{corpCode}' -> '/api/v1/finances/{corpCode}'

--- adam/src/main/java/com/finance/adam/controller/FinanceDataController.java
+++ adam/src/main/java/com/finance/adam/controller/FinanceDataController.java
@@ -1,35 +1,46 @@
 package com.finance.adam.controller;
 
+import com.finance.adam.dto.StepOneStockInfoDTO;
+import com.finance.adam.openapi.dart.vo.OpenDartReportExtractedDTO;
 import com.finance.adam.repository.financeinfo.dto.FinanceInfoDTO;
 import com.finance.adam.dto.KrxCorpListResponse;
 import com.finance.adam.service.FinanceDataService;
+import com.finance.adam.service.ScreeningService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/finances")
 public class FinanceDataController {
 
     private FinanceDataService financeDataService;
+    private ScreeningService screeningService;
 
-    public FinanceDataController(FinanceDataService financeDataService){
-        this.financeDataService = financeDataService;
+    @GetMapping("/screening")
+    public List<StepOneStockInfoDTO> getStepOneStockInfoList(){
+        return screeningService.getStepOneStockInfoList();
     }
 
-    @GetMapping("/stockInfos")
+    @GetMapping("/stocks")
     public List<KrxCorpListResponse> getCorpInfoList(){
         List<KrxCorpListResponse> result = financeDataService.getKrxCorpInfo();
         return result;
     }
 
-    @GetMapping("/financeInfo/{corpCode}")
+    @GetMapping("/{corpCode}")
     public List<FinanceInfoDTO> getFinanceInfos(@PathVariable String corpCode, @RequestParam int startYear, @RequestParam int endYear){
         List<FinanceInfoDTO> result = financeDataService.getFinanceInfos(corpCode, startYear, endYear);
+        return result;
+    }
+
+    @GetMapping("/reports/{corpCode}")
+    public List<OpenDartReportExtractedDTO> getReports(@PathVariable String corpCode){
+        List<OpenDartReportExtractedDTO> result = financeDataService.getReports(corpCode);
         return result;
     }
 }
