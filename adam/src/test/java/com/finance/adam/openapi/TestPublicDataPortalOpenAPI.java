454d4091809e88e8d12e71a2e5c1f71cf340824b
JMsuper <tjsqls2067@naver.com>
2024-07-27 18:43:18 +0900

    [Fix] 공공데이터포털API 응답값에 해당하는 필드를 응답DTO에 추가

--- adam/src/test/java/com/finance/adam/openapi/TestPublicDataPortalOpenAPI.java
+++ adam/src/test/java/com/finance/adam/openapi/TestPublicDataPortalOpenAPI.java
@@ -1,11 +1,14 @@
 package com.finance.adam.openapi;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.finance.adam.openapi.publicdataportal.PublicDataPortalOpenAPI;
 import com.finance.adam.openapi.publicdataportal.vo.KrxItemInfo;
+import com.finance.adam.openapi.publicdataportal.vo.KrxResponseBody;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.List;
@@ -13,10 +16,18 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+@SpringBootTest(classes = {
+        PublicDataPortalOpenAPI.class, ObjectMapper.class, RestTemplate.class
+})
 public class TestPublicDataPortalOpenAPI {
 
     @Autowired
     PublicDataPortalOpenAPI publicDataPortalOpenAPI;
+
+    @Test
+    void getKrxItemInfoMap(){
+        Map<String, KrxItemInfo> map = publicDataPortalOpenAPI.getKrxItemInfoMap();
+        System.out.println();
+    }
 
 }
